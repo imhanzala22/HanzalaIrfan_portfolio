f3f4cbe87878edf08bf4ca88014f9098108eff2e
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2021-07-28 18:33:36 +0500

    home page done

--- hanzalairfan/src/components/home/home.jsx
+++ hanzalairfan/src/components/home/home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './home.css';
-import image from '../images/image/user.png'
+import image from '../../Assets/image/user.png'
 const Home = () => {
     return (
         
-    <div  className='container' >
+    <div  className='container home' >
        <div className='flexuse'>
         <div className='left_box'>
+            <div className='text1'>hey,  My Name is</div>
+            <div className='text2'> Hanzala Irfan </div>
+            <div className='text3'><span> MERN </span> Stack Developer</div>
+            <button className='btn'>Explore more</button>
             
-        <h3> hey</h3>
-        <h2>My name is</h2>
-        <h1>HANZALA IRFAN</h1>
-        <h3>Mern Stack developer</h3>
 
         </div>
         <div className='right_Box'>
