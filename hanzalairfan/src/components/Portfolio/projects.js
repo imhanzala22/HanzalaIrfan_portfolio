6bdd04cafb85d6dbb8a4de7748cc6a3e859e50f1
imhanzala22 <71677063+imhanzala22@users.noreply.github.com>
2021-09-25 00:09:44 +0500

    final changes after some issues

---
+++ hanzalairfan/src/components/Portfolio/projects.js
@@ -0,0 +1,66 @@
+import img1 from "../../Assets/image/1.JPG";
+import img2 from "../../Assets/image/2.JPG";
+import img3 from "../../Assets/image/3.JPG";
+import img4 from "../../Assets/image/4.JPG";
+import img5 from "../../Assets/image/5.JPG";
+import img6 from "../../Assets/image/6.JPG";
+import img7 from "../../Assets/image/7.JPG";
+import img8 from "../../Assets/image/8.JPG";
+export const projects =[
+    {
+        id :1,
+        title: "SOFA HOUSE",
+        desc:"This is website for sofa store. I used reactjs and html,css and react bootstrap to develop it",
+        alignment: "left",
+        img: img1
+    },
+    {
+        id :2,
+        title: "DesireTech Computers",
+        desc:"This is website for E-commerce store. I used html,css reactjs to develop it and for backend it used fake Api",
+        alignment: "right",
+        img: img2
+    },
+    {
+        id :3,
+        title: "DesireTech Computers Admin Panel",
+        desc:"This is website for E-commerce store. I used html,css and reactjs to develop it.",
+        alignment: "left",
+        img: img3
+    },
+    {
+        id :4,
+        title: "MY Shop",
+        desc:"This is website for MY SHOP store. I used html,css and bootstrap to develop it. It was frontend only",
+        alignment: "left",
+        img: img4
+    },
+    {
+        id :5,
+        title: "MY Cart",
+        desc:"This is website for MY cart store. I used html,css and bootstrap to develop it. It get rating from users and it was a complete frontend",
+        alignment: "left",
+        img: img5
+    },
+    {
+        id :6,
+        title: "Mirror",
+        desc:"This is website for mirror newspaper. I used html,css and bootstrap to develop it.",
+        alignment: "left",
+        img: img6
+    },
+    {
+        id :7,
+        title: "Calories checker calculator",
+        desc:"This is website for calories check calculator. I used jquery, HTML and Css",
+        alignment: "left",
+        img: img7
+    },
+    {
+        id :8,
+        title: "Edruka cources",
+        desc:"This is website for E-commerce store. I used html,css,bootstrap,mongoDB,Express to develop it. MongonDB was also used as main part of it. Routers and modules are working accurately",
+        alignment: "left",
+        img: img8
+    }
+];
